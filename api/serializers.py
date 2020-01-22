5a45214c132fc2f426f530c95313d71afea98093
margaret254 <maggiemwas91@gmail.com>
2020-01-22 17:43:51 +0300

    Add news serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from .models import New
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,3 +14,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         validated_data['password'] = make_password(validated_data['password'])
         return super(UserSerializer, self).create(validated_data)
 
+class NewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = New
+        fields = ('id','title','desc','new_image','pub_date')
+        
