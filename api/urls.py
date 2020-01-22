e7defbacb6df5a3af176872d6368e48ef157f67a
margaret254 <maggiemwas91@gmail.com>
2020-01-22 17:44:12 +0300

    Add news urls

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,20 @@
-from django.urls import include, path
+from django.urls import include, path,re_path
 from rest_framework import routers
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'news', views.NewViewSet)
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    re_path(r'^', include(router.urls)),
+    re_path(r'^auth/',ObtainAuthToken.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
