2ddd59ff9dc61589a2ee51e67fad21559a55e378
margaret254 <maggiemwas91@gmail.com>
2020-01-22 17:43:33 +0300

    Add a news model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class New(models.Model):
+    title = models.CharField(max_length=30)
+    desc = models.CharField(max_length=30)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    new_image = models.ImageField(upload_to = 'images/')
+
