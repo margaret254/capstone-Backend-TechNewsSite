1dc15dd0df35a17f20049a63a4c62128d6ffeeab
margaret254 <maggiemwas91@gmail.com>
2020-01-24 12:20:55 +0300

    Add a post method

--- api/views.py
+++ api/views.py
@@ -15,3 +15,11 @@ class UserViewSet(viewsets.ModelViewSet):
 class NewViewSet(viewsets.ModelViewSet):
     queryset = New.objects.all()
     serializer_class = NewSerializer
+
+    def new(self,request):
+        serializer = NewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
